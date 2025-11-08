ecc7cc0c2612fe2d03b4e625b6808d919209b9d9
brayanM2108 <brayan.acevedo28@gmail.com>
2025-11-07 23:13:29 -0500

    Add `SpotifyController` and `SpotifyApi` to handle Spotify authorization and callback endpoints.

---
+++ src/main/java/com/melo/vibyn/spotify/infrastructure/api/SpotifyApi.java
@@ -0,0 +1,14 @@
+package com.melo.vibyn.spotify.infrastructure.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
+
+public interface SpotifyApi {
+
+    ResponseEntity<String> getUri(@RequestParam UUID userId);
+
+    ResponseEntity<String> callback( @RequestParam String code,
+                                     @RequestParam("state") UUID userId);
+}
---
+++ src/main/java/com/melo/vibyn/spotify/infrastructure/api/SpotifyController.java
@@ -0,0 +1,35 @@
+package com.melo.vibyn.spotify.infrastructure.api;
+
+import com.melo.vibyn.common.mediator.Mediator;
+import com.melo.vibyn.spotify.application.command.callback.CallbackSpotifyRequest;
+import com.melo.vibyn.spotify.application.command.connect.AuthorizationSpotifyRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/spotify")
+public class SpotifyController implements SpotifyApi{
+
+    private final Mediator mediator;
+
+    @Override
+    @GetMapping("/connect")
+    public ResponseEntity<String> getUri(@RequestParam UUID userId) {
+        String url = mediator.dispatch(new AuthorizationSpotifyRequest(userId));
+        return ResponseEntity.ok(url);
+    }
+
+    @Override
+    @GetMapping("/callback")
+    public ResponseEntity<String> callback(@RequestParam String code,
+                                           @RequestParam("state") UUID userId) {
+        String result = mediator.dispatch(new CallbackSpotifyRequest(code, userId));
+        return ResponseEntity.ok(result);
+    }
+}
