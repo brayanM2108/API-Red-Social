f77d4df9272d8f0a2babd29e06e37e24f23aad9b
brayanM2108 <brayan.acevedo28@gmail.com>
2025-11-12 16:37:41 -0500

    Rename `SpotifyMusicPort` to `SpotifySearchPort` and its implementation for better naming consistency.

--- src/main/java/com/melo/vibyn/spotify/application/port/SpotifySearchPort.java
+++ src/main/java/com/melo/vibyn/spotify/application/port/SpotifySearchPort.java
@@ -4,7 +4,9 @@ import com.melo.vibyn.spotify.infrastructure.api.dto.TrackDto;
 import java.util.List;
 import java.util.UUID;
 
-public interface SpotifyMusicPort {
+public interface SpotifySearchPort {
 
     List<TrackDto> findSearchTracks(UUID userId, String query);
+
+
 }
