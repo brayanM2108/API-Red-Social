3bfdc765fd8f790b6c36a7d5cf1628b3a30e9073
brayanM2108 <brayan.acevedo28@gmail.com>
2025-11-11 21:40:21 -0500

    Add `SpotifyMusicPort` interface

---
+++ src/main/java/com/melo/vibyn/spotify/infrastructure/persistence/port/SpotifyMusicPort.java
@@ -0,0 +1,10 @@
+package com.melo.vibyn.spotify.infrastructure.persistence.port;
+
+import com.melo.vibyn.spotify.infrastructure.api.dto.TrackDto;
+import java.util.List;
+import java.util.UUID;
+
+public interface SpotifyMusicPort {
+
+    List<TrackDto> findSearchTracks(UUID userId, String query);
+}
