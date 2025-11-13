996113e8e8a59e13abfdf87a18a54ccabff6e1a4
brayanM2108 <brayan.acevedo28@gmail.com>
2025-11-13 18:31:20 -0500

    Add `tracksIds` field to `PostCreateDto` with default to empty set

--- src/main/java/com/melo/vibyn/post/infrastructure/api/dto/PostCreateDto.java
+++ src/main/java/com/melo/vibyn/post/infrastructure/api/dto/PostCreateDto.java
@@ -2,15 +2,25 @@ package com.melo.vibyn.post.infrastructure.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
+import java.util.Collections;
+import java.util.Set;
 import java.util.UUID;
 
 public record PostCreateDto(
 
        @NotBlank(message = "the title cannot be blank")
        String title,
+
        @NotBlank(message = "the content cannot be blank")
        String content,
+
        @NotNull(message = "the userId cannot be null")
-       UUID userId
-) {}
+       UUID userId,
+
+       Set <String> tracksIds
+
+) {
+    public PostCreateDto {
+        tracksIds = tracksIds != null ? tracksIds : Collections.emptySet();
+    }
+}
