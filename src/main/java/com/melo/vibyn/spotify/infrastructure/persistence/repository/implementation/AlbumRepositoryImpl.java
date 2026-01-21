1c24be7d162a48de374c38ce401e72928caefe57
brayanM2108 <brayan.acevedo28@gmail.com>
2026-01-21 13:06:23 -0500

    Add 'AlbumRepositoryImpl' implementing AlbumRepositoryPort for album persistence

---
+++ src/main/java/com/melo/vibyn/spotify/infrastructure/persistence/repository/implementation/AlbumRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.melo.vibyn.spotify.infrastructure.persistence.repository.implementation;
+
+import com.melo.vibyn.spotify.domain.entity.AlbumDomain;
+import com.melo.vibyn.spotify.domain.port.AlbumRepositoryPort;
+import com.melo.vibyn.spotify.infrastructure.persistence.entity.AlbumEntity;
+import com.melo.vibyn.spotify.infrastructure.persistence.mapper.AlbumEntityMapper;
+import com.melo.vibyn.spotify.infrastructure.persistence.repository.JpaAlbumRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class AlbumRepositoryImpl implements AlbumRepositoryPort {
+
+    private final JpaAlbumRepository albumRepository;
+    private final AlbumEntityMapper albumMapper;
+
+    @Override
+    public AlbumDomain save(AlbumDomain albumDomain) {
+
+        AlbumEntity albumEntity = albumMapper.toEntity(albumDomain);
+        AlbumEntity save = albumRepository.save(albumEntity);
+        return albumMapper.toDomain(save);
+
+    }
+
+    @Override
+    public Optional<AlbumDomain> findBySpotifyId(String spotifyId) {
+        return albumRepository.findBySpotifyAlbumId(spotifyId)
+                .map(albumMapper::toDomain);
+    }
+}
